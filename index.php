e2a91df3b3903f1e3b7e27d11b71e32e9ce40ab4
NoremacSkich <noremacskich@gmail.com>
2014-01-20 23:58:45 -0600

    Added slick-nav, applied when screen is small
    Added:
    1 /libs/java/slick-nav/
    2  ^jquery.slicknav.js
    3  ^jquery.slicknav.min.js
    4  ^MIT-LICENSE.txt
    5  ^README.md
    6  ^slicknav.css
    
    Reasons:
    1-6 slick-nav looks like it will make navigation menus easy
    
    Modified:
    1 navigation.php : createNav()
    2 index.php
    
    Reason:
    1 Using the menu template from slicknav.com to test out slick-nav
    2 Moved the core.css to be the last loaded so that it overides
    everything else.
    
    Deleted:
    1 /History/History.log
    
    Reasons:
    1 Decided not to use it, will just keep track of beforePush.log
    
    Thoughts:
    1 Nav Menu needs to have a style applied to it for tablet display and
    desktop display.

--- index.php
+++ index.php
@@ -7,19 +7,22 @@
 		<head>
 			<title>Nanocon</title>
 				<?php 
-					newScript('/libs/java/jquery-1.10.2.min.js');
-					newCss('/css/core.css');
+					newScript('libs/java/jquery-1.10.2.min.js');
+
 					// Jquery Mobile Library via CDN-hosted files
 					newCss('http://code.jquery.com/mobile/1.4.0/jquery.mobile-1.4.0.min.css');
 					newScript('http://code.jquery.com/jquery-1.9.1.min.js');
 					newScript('http://code.jquery.com/mobile/1.4.0/jquery.mobile-1.4.0.min.js');
 					// end Jquery Mobile Library
+					newScript('libs/java/slick-nav/jquery.slicknav.js');
+					newCss('libs/java/slick-nav/slicknav.css');
+					newCss('css/core.css');
 				?>
 				<!-- Needed for Jquery Mobile Library -->
 				<meta name="viewport" content="width=device-width, initial-scale=1">
 		</head>
 		<body data-role="page">
-			<header data-role="header">
+			<header data-role="header" id="header">
 				<h1>Nanocon 2014</h1>
 				
 				<?php 
--- navigation.php
+++ navigation.php
@@ -22,13 +22,44 @@
 	 * 
 	 */
 	function createNav(){
-		echo '<div id="nav" data-role="navbar">';
+		/*echo '<div id="nav" data-role="navbar">';
 		echo '	<ul>';
 		newPageLink('#','Option1');
 		newPageLink('#','Option2');
 		newPageLink('#','Option3');
 		echo '	</ul>';
 		echo '</div>';
-
+*/
+		echo <<<endecho
+		<ul id="menu2">
+		    <li>Parent 1
+		        <ul>
+		            <li><a href="#">item 3</a></li>
+		            <li>Parent 3
+		                <ul>
+		                    <li><a href="#">item 8</a></li>
+		                    <li><a href="#">item 9</a></li>
+		                    <li><a href="#">item 10</a></li>
+		                </ul>
+		            </li>
+		            <li><a href="#">item 4</a></li>
+		        </ul>
+		    </li>
+		    <li><a href="#">item 1</a></li>
+		    <li>non-link item</li>
+		    <li>Parent 2
+		        <ul>
+		            <li><a href="#">item 5</a></li>
+		            <li><a href="#">item 6</a></li>
+		            <li><a href="#">item 7</a></li>
+		        </ul>
+		    </li>
+		</ul>
+		<script>
+		$('#menu2').slicknav({
+			label:'Nanocon - 2014'
+		});
+		</script>
+endecho;
 	}
 ?>
