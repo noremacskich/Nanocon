3a62ce51a0c402859024211ea29e83473bb0bf29
NoremacSkich <noremacskich@gmail.com>
2014-03-05 16:31:59 -0600

    eventInfoDB & setBasicInfo stub
    Created:
    1	eventinfo database
    2	eventinfo : BasicInfo
    3	database.php : setBasicInfo($basicInfo)
    
    Reasons:
    1	This database will hold all data related to events.
    2	This table will hold the basic info fields such as names, addresses, emails,
    	and phone numbers
    3	This will put the basic information into the database.  It will not check
    	for formatting of the string, but it will do a security check on the
    	parameters before it inserts them into the database.
    
    --------------
    
    Notes:
    1	Will probably have to rethink how I am going to do the database connections.

--- database.php
+++ database.php
@@ -158,5 +158,56 @@
 		}
 		
 	}
-
+	
+	/**@fun setBasicInfo($name, $address, $city, $state, $zip, $phone, $email)
+	 * 	^	This will put the basic information into the database.  It will
+	 * 		not check for formatting of the string, but it will do a security
+	 * 		check on the parameters before it inserts them into the database.
+	 * 		 
+	 * @author NoremacSkich | 2014-3-5
+	 * 
+	 * @param $basicInfo | Array
+	 *	 	^	This array contains all the info for a person.
+	 *
+	 * 	 @element $name | string
+	 *	 	^	This is the full name of the person.
+	 *	 
+	 *	 @element $address | string
+	 *		^	This is their street address
+	 *	 
+	 *	 @element $city | string
+	 *		^	This is the city
+	 *	 	N	The city and zip should point to the same location.
+	 *	 
+	 *	 @element $state | string
+	 *		^	This can either be the full name of the state, or the 
+	 *	 		abbreviation of the state.
+	 *		N	This should probably just be the abbreviation of the state.
+	 *	 
+	 *	 @element $zip | Integer
+	 *	 	^	This is their zip code
+	 *		N	There might be an issue, since there are zip codes out there 
+	 *	 		with an dash in it.
+	 *	 
+	 *	 @element $phone | Phone
+	 *		^	This is their phone number.
+	 *		
+	 *	 @element $email | Email
+	 *		^	This is their email address.
+	 * 
+	 * @note NoremacSkich | 2014-3-5
+	 *	^	We might run into issues with the zip code, since there are two 
+	 * 		types, normal, and then the extended zip code, which has a dash
+	 * 		in it.
+	 * 
+	 * @note NoremacSkich | 2014-3-5
+	 *	^	Probably should stick with state abbreviations.
+	 * 
+	 * @note NoremacSkich | 2014-3-5
+	 *	^	Formatting the text is not done in this function.
+	 */
+	 function setBasicInfo($basicInfo){
+	 	// Stub
+		
+	 }
 ?>
